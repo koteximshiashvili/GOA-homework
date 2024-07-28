563201ea941ea2b43c52b79e1fcd5e6e3b8e6e5c
koteximshiashvili <tamunaputkaradze92@gmail.com>
2024-07-28 07:55:36 +0300

    thirtyfourth commit

--- day 34/homework/homework.py
+++ day 34/homework/homework.py
@@ -14,3 +14,34 @@ print(find_short("bitcoin take over the world maybe who knows perhaps"))
 #ამ კოდში S ცვლადს სფეისებთან ბრჭყალები დაეწერება შემდეგ შევქმენით ცვლადი word_lenი word_lenში იქნება 7 რადგან 
 #პირველი სიტყვა არის 7 ასოთი შემდეგ ფუნქცია ამოწმებს შემდეგი სიტყვა ასოებით ნაკლებია თუ არა თუ არ არის ნაკლები
 #მაშინ გააგრძელებს ძებნას თუ არის ნაკლები მაშინ ამ რიცხვს შეიტანს word_lenში და ასე გააგრძელებს ბოლომდე.
+
+
+words = "hello! my name is kote" 
+print(words.split(" "))
+
+words = "hello! my surname is ximshiashvili" 
+print(words.split("i"))
+
+words = "im ,in ,goa" 
+print(words.split(","))
+
+words = "this is my homework" 
+print(words.split("h"))
+
+words = "i like reading" 
+print(words.split("i"))
+
+words = "to be continued" 
+print(words.split("n"))
+
+words = "i like proggraming" 
+print(words.split("g"))
+
+words = "i prefer web development" 
+print(words.split("e"))
+
+words = "i like making sites" 
+print(words.split("i"))
+
+words = "i like chess" 
+print(words.split("s"))
