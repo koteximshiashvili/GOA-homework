34b7f5eb75a394abceae92b429ec907d7fb763db
koteximshiashvili <tamunaputkaradze92@gmail.com>
2024-10-06 08:06:18 +0300

    first commit

--- day 01/lesson1.py
+++ day 01/lesson1.py
@@ -1,34 +1,85 @@
-#name = "kote"
-#name aris cvladi
-# = aris cvladistvis mnishvnelobis mimniWebeli  simbolo
-# "nika" aris cvladisTvis mnishvneloba
+from turtle import*
+shape("turtle")
+speed(50)
+width(7)
+color("purple")
+forward(200)
+#we want to paint a house
 
-surname = "khimshiashvili"
-
-# print(name)
-#print funqcias gadaecema ekranze gamosatani obieqti
-
-name = "kote" # es aris string tipis cvladi
-age = 11 # es aris int integer mTeli ricxvi
-height = 145 #es aris float tipis cvladi atwiladi
-# boolean (bool) tipis cvladi
-
-knows_programming = True #true an false
-is_ugly = False #snakecase(ubralod weris stili standartulad)
-
-isugly = False #javaskriptuli comelace
+#step 1 draw a square
+left(90)
 
 
-#print(name + "" + surname)
+forward(200)
+left(90)
 
-#stringi aris brWyalebshi moqceuli simboloebi
-#print(name + age)
 
-print(type(age))
-print(type(name))
-print(type(surname))
-print(type(height))
-print(type(knows_programming))
+forward(200)
+left(90)
 
-print(name + surname + str(height) + str(knows_programming) + str(age))
+forward(200)
+left(90)
 
+
+
+
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+penup()
+goto(200, 120)
+pendown()
+
+
+
+color("blue")
+begin_fill()
+left(300)
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(60)
+end_fill()
+
+penup()
+goto(0, 120)
+pendown()
+
+color("blue")
+begin_fill()
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+end_fill()
+exitonclick()
