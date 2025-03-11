6d640915b6fc7e370c08737d71f8451ec1886bfc
koteximshiashvili <tamunaputkaradze92@gmail.com>
2025-03-11 11:50:28 +0200

    commit

--- day 127/homework/task1.js
+++ day 127/homework/task1.js
@@ -1,6 +1,8 @@
-fetch("https://jsonplaceholder.typicode.com/users").then(res => { // აქ ვიყენებტ fetchს რომ საიტიდან გამოვიტანოთ ინფორმაცია
+fetch("https://jsonplaceholder.te.com/users").then(res => { // აქ ვიყენებტ fetchს რომ საიტიდან გამოვიტანოთ ინფორმაცია
+    console.log(res)
     if (res.status === 404){ // შემდეგ ვამოწმებთ იმას თუ შედეგი რაც გამოვიდა ამის სტატუსი არის 404
         document.body.style.background = "url('https://cdn.discordapp.com/attachments/1237784686269304932/1346553213083586711/Screenshot_2025-01-19_at_22.11.54.png?ex=67c89ae7&is=67c74967&hm=b3d07a8c650ce8ff1c49e692a3efd7ff75fe26fc387f409988624c34273ff9eb&')"; // თუ ასეა მაშინ ვუცვლით ბოდის ბექგრაუნდ იმეჯს
+        return Promise.reject("Error 404");
     }
     else if(res.ok){ // აქ ვამოწმებთ თუ შედეგი რაც გამოვიდა ამის ok ფუნქციას რომელსაც გამოაქვს ან true ან false
         document.body.style.background = "green"; // თუ true არის მაშინ ბოდის ვუცვლით ბექგრაუნდს
