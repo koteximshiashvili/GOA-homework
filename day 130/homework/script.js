a0c5168b1be1bbde2209dd28e4eb7238566417c2
koteximshiashvili <tamunaputkaradze92@gmail.com>
2025-03-15 07:31:23 +0200

    commit

---
+++ day 130/homework/script.js
@@ -0,0 +1,34 @@
+async function getJoke(category){
+    let fetchedData = await fetch("https://v2.jokeapi.dev/joke/Any?safe-mode");
+    if(!fetchedData.ok){
+        throw new Error("Error.")
+    }
+    let data = await fetchedData.json();
+    if(data.category.toLowerCase() !== category.toLowerCase()){
+        document.querySelector('p').innerHTML = 'not the right category found';
+    }
+    else if('joke' in data){
+            document.querySelector('p').innerHTML = data.joke;
+            document.querySelector('span').innerHTML = '';
+        }
+    else{
+            document.querySelector('p').innerHTML = data.setup;
+            document.querySelector('span').innerHTML = data.delivery;
+    }
+}
+async function getAdvice(){
+    let fetchedData = await fetch("https://api.adviceslip.com/advice");
+    if(!fetchedData.ok){
+        throw new Error("Error.")
+    }
+    let data = await fetchedData.json();
+    document.querySelector('div').innerHTML = data.slip.advice;
+}
+getAdvice()
+document.querySelector('.advice').addEventListener('click', function(){
+    getAdvice()
+})
+document.querySelector('.joke').addEventListener('click', function(){
+    let category = document.querySelector('input').value;
+    getJoke(category)
+});
