701ec54629849cc04e258387719477ebb77e5a46
koteximshiashvili <tamunaputkaradze92@gmail.com>
2025-04-22 19:33:43 +0300

    commit

--- day 144/homework/my-react-app/src/components/Calculator.jsx
+++ day 144/homework/my-react-app/src/components/Calculator.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 function Calculator(){
    const [count, setCount] = useState(0);
    const [numberOne, setNumberOne] = useState(0);
