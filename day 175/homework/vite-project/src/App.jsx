b8123b65fd8c3213c48bf41bbfdc8059e1f4bd11
koteximshiashvili <tamunaputkaradze92@gmail.com>
2025-06-23 19:05:55 +0300

    commit

---
+++ day 175/homework/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Scroll from "./components/Scroll";
+import Form from "./components/Form";
+import Timer from "./components/Timer";
+function App() {
+  return (
+    <>
+      <Scroll />
+      <Form />
+      <Timer />
+    </>
+  )
+}
+
+export default App
